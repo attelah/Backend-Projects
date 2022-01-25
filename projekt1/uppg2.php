1ede9adb0879b4ed4a927722aeaa9186727d75d7
Kindstep <kindstep@arcada.fi>
2022-01-25 11:52:38 +0200

    satt in veckodags array

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -7,7 +7,9 @@
                 <?php
                 $tid = date("d.M.Y");
                 print("idag är det ".$tid);
+                $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
                 ?>
+
 
             </article>
 
