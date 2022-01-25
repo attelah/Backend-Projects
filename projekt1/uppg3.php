ebfecf02b839f89cebf8e732ebfe060713cdc4bd
KimEngblom <engbloki@arcada.fi>
2022-01-25 11:40:52 +0200

    en del frammåt men ett error i ifsatsen troligen

--- projekt1/uppg3.php
+++ projekt1/uppg3.php
@@ -4,21 +4,33 @@
     <form action="index.php" method="POST">
         Dag: <input type="text" name="dag"> <br>
         Månad: <input type="text" name="manad"><br>
+        År: <input type="text" name="ar"><br>
         <input type="submit" name="skicka" value="Räkna">
     </form>
     <?php 
-    //hämta inmatad data
-    $dag = $_GET["dag"];
+    if(isset($_GET["dag"]) && !empty($_GET["manad"])){
+        //hämta inmatad data
+        $dag = $_GET["dag"];
+        $manad = $_GET["manad"];
+        $ar = $_GET["ar"];
+        
+        if(($dag > 0) && ($dag <= 30)){
 
-    //hitta tiden nu
-    $tidNu = time();
-    //Skapa en timestamp baserat på inmatning
-    $givenTid = mktime(12,0,0,$manad, $dag, 2022);
+        //hitta tiden nu
+        $tidNu = time();
+        //Skapa en timestamp baserat på inmatning
+        $givenTid = mktime(12,0,0,$manad, $dag, 2022);
 
-    //använd floor() för att avrunda nedåt 
+        //använd floor() för att avrunda nedåt 
 
-    //skriv ut variabler
-    print("det är " . $dag . "till det inmatade datumet");
+        //skriv ut variabler
+        print("det är " . $dag . "till det inmatade datumet");
+
+        }
+        else{
+            print("din inmatning är skum?");
+        }
+    }
     ?>
 
 
