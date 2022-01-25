f4844fc96c1765b834457378cd132880b6793cc5
attelah <lahepela@arcada.fi>
2022-01-25 12:12:42 +0200

    Uppg 1 fixad

--- projekt1/uppg1.php
+++ projekt1/uppg1.php
@@ -1,10 +1,16 @@
 <article>
-    <h2>uppg 1 - Användardata</h2>
-    <p>Användarinfo</p>
+    <h2>Uppg 1 - Användar och serverdata</h2>
+    <p><b>Användar & Serverinfo</b></p>
     <?php 
-        print ("Servern snurrar på port:" . $_SERVER["SERVER_PORT"] );
-        // phpinfo(); 
+        // phpinfo();
+        echo nl2br("Användare: " . $_SERVER["REMOTE_USER"]);
+        echo nl2br("<br>Användarens IP: " . $_SERVER["REMOTE_ADDR"]);
+        echo nl2br("<br>Serverns namn: " . $_SERVER["SERVER_NAME"]);
+        echo nl2br("<br>Serverns IP: " . $_SERVER["SERVER_ADDR"]);
+        echo nl2br("<br>Servern snurrar på port: " . $_SERVER["SERVER_PORT"]);
+        echo nl2br("<br>Apache versionen: " . apache_get_version());
+        echo nl2br("<br>PHP versionen: " . phpversion());
     ?> 
-
+    
 </article>
 <div class="separator"></div>
