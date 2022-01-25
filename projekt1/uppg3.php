1a5283f98014eb0263a2e41be624435b4709c0fc
Kindstep <kindstep@arcada.fi>
2022-01-25 10:58:50 +0200

    Uppg 2

--- projekt1/uppg3.php
+++ projekt1/uppg3.php
@@ -1,6 +1,13 @@
 <article>
                 <h2>Uppg 3</h2>
                 <p>Användarinmatning</p>
+
+    <form action="index.php" method="get">
+    Dag: <input type="text" name="dag"><br>
+    Månad: <input type="text" name="dag"><br>
+    <input type="submit" name="skicka" value="Räkna">
+    </form>
+    
             </article>
 
             <div class="separator"></div>
