b25c3017d0b2f39b1bdcf99e6a148aafdb453279
attelah <lahepela@arcada.fi>
2022-02-02 12:01:55 +0200

    Profilbild & profilbild upload

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -1,16 +1,14 @@
 <article>
-                <h2>Uppg 2</h2>
-                <p>Tid och Datum</p>
+    <h2>Uppg 2</h2>
+    <p>Tid och Datum</p>
 
-                <h2>Uppg2</h2>
-                <p>tid och datum</p>
-                <?php
-                $tid = date("d.M.Y");
-                print("idag är det ".$tid);
-                $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
-                ?>
+    <?php
+    $tid = date("d.M.Y");
+    print("idag är det ".$tid);
+    $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
+    ?>
 
 
-            </article>
+</article>
 
-            <div class="separator"></div>
+<div class="separator"></div>
