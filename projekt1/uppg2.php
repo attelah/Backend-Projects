08aff847ef2fbaf45bdfd6d1ce75a5421e447ad7
Kindstep <kindstep@arcada.fi>
2022-02-03 18:08:33 +0200

    mera testande på u2

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -2,45 +2,45 @@
     <h2>Tid och datum - Uppg 2</h2>
 
     <?php
-    $tid = date("d.M.Y");
+    $tid = date("l.d.M.Y");
     print("idag är det ".$tid);
 
 
     $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
     $Months = array("Januari", "Februari", "Mars", "April", "Maj","Juni","Juli", "Augusti", "October", "September", "November","December");
-    $dag = [];
-    $manad = [];
+    $dag = date("l");
+    $manad = date("M");
 
-    if (date("l")==[0])
+    if ($dag==[0])
     {
-        $veckodagar = [0];
+       $dag = $veckodagar[0];
     }
-    if (date("l")==[1])
+    if ($dag==[1])
     {
-        $veckodagar = [1];
+        $dag = $veckodagar[1];
     }
-    if (date("l")==[2])
+    if ($dag==[2])
     {
-        $veckodagar = [2];
+        $dag = $veckodagar[2];
     }
-    if (date("l")==[3])
+    if ($dag==[3])
     {
-        $veckodagar = [3];
+        $dag = $veckodagar[3];
     }
-    if (date("l")==[4])
+    if ($dag==[4])
     {
-        $veckodagar = [4];
+        $dag = $veckodagar[4];
     }
-    if (date("l")==[5])
+    if ($dag==[5])
     {
-        $veckodagar = [5];
+        $dag = $veckodagar[5];
     }
-    if (date("l")==[6])
+    if ($dag==[6])
     {
-        $veckodagar = [6];
+        $dag = $veckodagar[6];
     }
 
-    print("idag är det ".$veckodag);
+    print("idag är det ".$dag);
 
 
     ?>
