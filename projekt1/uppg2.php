310d7f4acc9bb41ee62b2a3322d10b804e20e603
Kindstep <kindstep@arcada.fi>
2022-02-03 18:00:22 +0200

    veckodagar (testar en kod)

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -4,10 +4,43 @@
     <?php
     $tid = date("d.M.Y");
     print("idag är det ".$tid);
+
+
     $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
     $Months = array("Januari", "Februari", "Mars", "April", "Maj","Juni","Juli", "Augusti", "October", "September", "November","December");
-    $dag = date("l");
-    print("idag är det ".$dag);
+    $dag = [];
+    $manad = [];
+
+    if (date("l")==[0])
+    {
+        $veckodagar = [0];
+    }
+    if (date("l")==[1])
+    {
+        $veckodagar = [1];
+    }
+    if (date("l")==[2])
+    {
+        $veckodagar = [2];
+    }
+    if (date("l")==[3])
+    {
+        $veckodagar = [3];
+    }
+    if (date("l")==[4])
+    {
+        $veckodagar = [4];
+    }
+    if (date("l")==[5])
+    {
+        $veckodagar = [5];
+    }
+    if (date("l")==[6])
+    {
+        $veckodagar = [6];
+    }
+
+    print("idag är det ".$veckodag);
 
 
     ?>
