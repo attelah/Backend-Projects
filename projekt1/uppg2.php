7492b09655ea22076b0d5845298135138ca86549
Kindstep <kindstep@arcada.fi>
2022-02-03 18:36:46 +0200

    Testis

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -2,10 +2,7 @@
     <h2>Tid och datum - Uppg 2</h2>
 
     <?php
-    $tid = date("l.d.M.Y");
-    print("idag är det ".$tid);
-
-
+   
     $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
     $Months = array("Januari", "Februari", "Mars", "April", "Maj","Juni","Juli", "Augusti", "October", "September", "November","December");
     $dag = date("l");
@@ -39,8 +36,58 @@
     {
         echo "idag är det ".$veckodagar[6];
     }
-    
 
+    print (" den ".date("D")." ");
+
+    $manad = date("n");
+    if ($manad = 1)
+    {
+        echo $Months[0];
+    }
+    if ($manad = 2)
+    {
+        echo $Months[1];
+    }
+    if ($manad = 3)
+    {
+        echo $Months[2];
+    }
+    if ($manad = 4)
+    {
+        echo $Months[3];
+    }
+    if ($manad = 5)
+    {
+        echo $Months[4];
+    }
+    if ($manad = 6)
+    {
+        echo $Months[5];
+    }
+    if ($manad = 7)
+    {
+        echo $Months[6];
+    }
+    if ($manad = 8)
+    {
+        echo $Months[7];
+    }
+    if ($manad = 9)
+    {
+        echo $Months[8];
+    }
+    if ($manad = 10)
+    {
+        echo $Months[9];
+    }
+    if ($manad = 11)
+    {
+        echo $Months[10];
+    }
+    if ($manad = 12)
+    {
+        echo $Months[11];
+    }
 
     ?>
 
