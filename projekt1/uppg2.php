9c5614ec733f077e35c58bf3e37155bf583ffe67
KimEngblom <engbloki@arcada.fi>
2022-02-03 10:27:35 +0200

    Månads array inlagd

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -5,6 +5,7 @@
     $tid = date("d.M.Y");
     print("idag är det ".$tid);
     $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
+    $Months = array("Januari", "Februari", "Mars", "April", "Maj","Juni","Juli", "Augusti", "October", "September", "November","December");
     $dag = date("l");
     print("idag är det ".$dag);
 
