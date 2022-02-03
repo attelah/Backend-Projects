f532624eaa8e892a58e401c0bf7e0cfa0a28d14e
Kindstep <kindstep@arcada.fi>
2022-02-03 18:24:59 +0200

    Fixar skrivfel

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -13,31 +13,31 @@
 
     if ($dag=="Sunday")
     {
-        echo "idag är det ".$veckodag[0];
+        echo "idag är det ".$veckodagar[0];
     }
     if ($dag=="Monday")
     {
-        echo "idag är det ".$veckodag[1];
+        echo "idag är det ".$veckodagar[1];
     }
     if ($dag=="Tuesday")
     {
-        echo "idag är det ".$veckodag[2];
+        echo "idag är det ".$veckodagar[2];
     }
     if ($dag=="Wednesday")
     {
-        echo "idag är det ".$veckodag[3];
+        echo "idag är det ".$veckodagar[3];
     }
     if ($dag=="Thursday")
     {
-        echo "idag är det ".$veckodag[4];
+        echo "idag är det ".$veckodagar[4];
     }
     if ($dag=="Friday")
     {
-        echo "idag är det ".$veckodag[5];
+        echo "idag är det ".$veckodagar[5];
     }
     if ($dag=="Saturday")
     {
-        echo "idag är det ".$veckodag[6];
+        echo "idag är det ".$veckodagar[6];
     }
     
 
