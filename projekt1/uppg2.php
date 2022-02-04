c0def979cf320772336d777cffa2e7b89434965c
Kindstep <kindstep@arcada.fi>
2022-02-04 09:38:04 +0200

    Förkortade koden en hel del

--- projekt1/uppg2.php
+++ projekt1/uppg2.php
@@ -5,94 +5,16 @@
    
     $veckodagar = array("Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag","Fredag","Lördag");
     $Months = array("Januari", "Februari", "Mars", "April", "Maj","Juni","Juli", "Augusti", "October", "September", "November","December");
-    $dag = date("l");
+    $dag = date("w");
     $week = date("W");
-
-    if ($dag=="Sunday")
-    {
-        echo "Idag är det ".$veckodagar[0];
-    }
-    if ($dag=="Monday")
-    {
-        echo "Idag är det ".$veckodagar[1];
-    }
-    if ($dag=="Tuesday")
-    {
-        echo "Idag är det ".$veckodagar[2];
-    }
-    if ($dag=="Wednesday")
-    {
-        echo "Idag är det ".$veckodagar[3];
-    }
-    if ($dag=="Thursday")
-    {
-        echo "Idag är det ".$veckodagar[4];
-    }
-    if ($dag=="Friday")
-    {
-        echo "Idag är det ".$veckodagar[5];
-    }
-    if ($dag=="Saturday")
-    {
-        echo "Idag är det ".$veckodagar[6];
-    }
-
-    print (" den ".date("j")." ");
-
     $manad = date("n");
+    $datnum = date("j");    
 
-    if ($manad == 1)
-    {
-        echo $Months[0];
-    }
-    if ($manad == 2)
-    {
-        echo $Months[1];
-    }
-    if ($manad == 3)
-    {
-        echo $Months[2];
-    }
-    if ($manad == 4)
-    {
-        echo $Months[3];
-    }
-    if ($manad == 5)
-    {
-        echo $Months[4];
-    }
-    if ($manad == 6)
-    {
-        echo $Months[5];
-    }
-    if ($manad == 7)
-    {
-        echo $Months[6];
-    }
-    if ($manad == 8)
-    {
-        echo $Months[7];
-    }
-    if ($manad == 9)
-    {
-        echo $Months[8];
-    }
-    if ($manad == 10)
-    {
-        echo $Months[9];
-    }
-    if ($manad == 11)
-    {
-        echo $Months[10];
-    }
-    if ($manad == 12)
-    {
-        echo $Months[11];
-    }
+    $day = $veckodagar[$dag-1];
+    $month = $Months[$manad-1];
 
-    print (", Vecka ".$week.". Vilken härlig dag!");
+    print ("idag är det ".$day." den " .$datnum." " .$month. ", Vecka ".$week.". Vilken härlig dag!");
     
-    //Denna vackra kodsnutt är Patriks, erkänner det här så de andra inte behöver skämmas. It works, det är det viktigaste! :D
     ?>
 
 </article>
