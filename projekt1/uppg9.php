835735baa6ba8b2e9aeb64fdb6bf29c29eead11b
KimEngblom <engbloki@arcada.fi>
2022-02-08 10:54:49 +0200

    uppg 9 100% klar

--- projekt1/uppg9.php
+++ projekt1/uppg9.php
@@ -1,5 +1,6 @@
 <article>
     <h2>Gästbok - Uppg 9</h2>
+    <!--formulär för blogg inkägget -->
     <form method="POST" action="">
         <label for="bloggText">Blogg text</label> <br>
         <textarea name="bloggText" id="bloggText" cols="35" rows="7"></textarea> <br>
@@ -7,26 +8,34 @@
     </form>
 
     <?php 
-    $bloggFileName = "bloggTexter.php";
-
+    //deklarerar filens namn i en variabel
+    $bloggFileName = "bloggTexter.txt";
+        //checkar att nån har tryckt på formulärets submit knapp
         if(isset($_POST['submitBloggText'])){
+            //Kollar upp vem usern är
             $postUser = $_SERVER["REMOTE_USER"];
+            //loggar tiden
             $postTimestamp = date("H:i");
+            //loggar datumet
             $postDate = date("d.m.Y");
+            //loggar texten och ser till att man inte kan skicka in skadliga HTML querries
             $BloggText = test_input( $_POST['bloggText']);
+            //skriver ut kodstycket i ett HTML format så som det skall se ut på sidan kan göra det finare med classer ;)
             $content = 
             "Användare: " . $postUser . ", Klockan: " . $postTimestamp . " Den " . $postDate ."<br> <br>" .
             $BloggText ." <br> <br> <br> <br>";
 
-            echo $content;
+            //echo $content;
+            //lägger till content in i filen så att den nyaste bloggen kommer först
+            $content .= file_get_contents($bloggFileName);
+            file_put_contents($bloggFileName, $content);
 
-            $fp = fopen($bloggFileName, 'a');
-            fwrite($fp, $content);
-            fclose($fp);
+            //refreshar sidan så att POST variabeln blir unset
+            header('location: '. $_SERVER['PHP_SELF'] );
 
-            //file_put_contents($bloggFileName, $content, FILE_APPEND);
+            
         }
-
+        //inkluderar text filen så att texten skrivs ut
         include $bloggFileName;
     
     ?>
