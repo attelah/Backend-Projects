9d6c4bd75b105fe6189cedb11135c673cbbe1cc6
attelah <lahepela@arcada.fi>
2022-02-18 14:24:06 +0200

    view profile

--- projekt2/view_profile.php
+++ projekt2/view_profile.php
@@ -1,24 +1,27 @@
 <article>
-    <h2>Här är din profil</h2>
-    <p>Du kan ändra på värdena för att uppdatera din annons</p>
+    <h2>MIN PROFIL</h2>
+    <p>Här kan du uppdatera din profil!</p>
 
     <?php
     // hämta profildata
         $username = test_input($_SESSION['username']);
-        $sql = "SELECT username,`password`,email,fullname,city,salary,preference,aboutme FROM annonser WHERE username = ?";
+        $sql = "SELECT id,username,`password`,email,fullname,city,salary,preference,aboutme FROM annonser WHERE username = ?";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$username]);
         ?>
         <!-- Alternative syntax för conditionals -->
         <?php if($row = $stmt->fetch(PDO::FETCH_ASSOC)) : ?>
+        <?php $userID = $row['id']; ?>
+        <?php $password = $row['password']; ?>
         <!-- Man kan också inkludera php trots alternative syntax -->
-        <?php print("Välkommen tillbaka " . $row['fullname'] . "<br>"); ?>
             <form action="profile.php" method="get">
             <!-- Om ni sätter php bland html, kom ihåg att inget syns innan print -->
         Användarnamn: <input type="text" name="username" value="<?=$row['username']?>"> <br>
-        Lösenord: <input type="text" name="password" value="<?=$row['password']?>"><br>
-        E-post: <input type="email" name="email" value="<?=$row['email']?>"><br>
-        <h2>Info till din profil:</h2><br>
+        E-post: <input type="email" name="email" value="<?=$row['email']?>"><br><br>
+        Uppdatera ditt lösenord<br>
+        Nuvarande Lösenordet: <input type="password" name="passwordOld"><br>
+        Nya Lösenordet:  <input type="password" name="passwordNew"><br><br>
+        <h2>MIN ANNONS</h2><br>
         <!-- Shorthand syntax för php preint sparar lite tecken -->
         Ditt hela namn: <input type="text" name="fullname" value="<?=$row['fullname']?>"><br>
         Hemstad: <input type="text" name="city" value="<?= $row['city']?>"><br>
@@ -26,16 +29,69 @@
         <label for="preference">Jag söker efter:</label>
         <select id="preference" name="preference">
         <!--https://stackoverflow.com/questions/3518002/how-can-i-set-the-default-value-for-an-html-select-element!-->
-            <option <?php if($row['preference'] == '1'){echo("selected");}?>>Män</option>
-            <option <?php if($row['preference'] == '2'){echo("selected");}?>>Kvinnor</option>
-            <option <?php if($row['preference'] == '3'){echo("selected");}?>>Män & Kvinnor</option>
-            <option <?php if($row['preference'] == '4'){echo("selected");}?>>Vad som hällst</option>
-            <option <?php if($row['preference'] == '5'){echo("selected");}?>>Vill inte specifiera</option>
+            <option value="1"<?php if($row['preference'] == '1'){echo("selected");}?>>Män</option>
+            <option value="2"<?php if($row['preference'] == '2'){echo("selected");}?>>Kvinnor</option>
+            <option value="3"<?php if($row['preference'] == '3'){echo("selected");}?>>Män & Kvinnor</option>
+            <option value="4"<?php if($row['preference'] == '4'){echo("selected");}?>>Vad som hällst</option>
+            <option value="5"<?php if($row['preference'] == '5'){echo("selected");}?>>Vill inte specifiera</option>
         </select><br>
         Berätta om dig själv: <input type="text" name="aboutme" value="<?=$row['aboutme']?>"><br>
-        <input type="hidden" name="page" value="modify">
+        <input type="hidden" name="page" value="modify"> <!--FIXA DET HÄR---------------------------------------------------------------------------------------------->
         <input type="submit" name="skicka" value="Spara Ändringar">
     </form>
+    
         <?php endif; ?>
+
+    <?php
+    if(!empty($_REQUEST['passwordOld']) && !empty($_REQUEST['passwordNew'])) {
+        $passwordNew = test_input($_REQUEST["passwordNew"]);
+        $passwordOld = test_input($_REQUEST["passwordOld"]);
+        if(hash("sha256", $passwordOld) === $password) {
+        $passwordNew = hash("sha256", $passwordNew);
+        $sql = "UPDATE annonser SET password='$passwordNew' WHERE id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$userID]);
+        if($stmt) {
+            print("Lösenordet uppdaterat!<br>");
+            header("Refresh:2; url=profile.php");
+        }
+        else {
+            print("An unexpected error has occurred");
+        }
+        }
+        else {
+            print("Ditt gamla lösenord stämmer inte!");
+        }
+    }
+    if(!empty($_REQUEST['username']) || !empty($_REQUEST['fullname']) || !empty($_REQUEST['city']) || !empty($_REQUEST['salary']) || !empty($_REQUEST['preference'])  || !empty($_REQUEST['aboutme']) || !empty($_REQUEST['preference']))
+    {
+    $username = test_input($_REQUEST["username"]);
+    $email = test_input($_REQUEST["email"]);
+    $fullname = test_input($_REQUEST["fullname"]);
+    $city = test_input($_REQUEST["city"]);
+    $salary = test_input($_REQUEST["salary"]);
+    $aboutme = test_input($_REQUEST["aboutme"]);
+    $preference = test_input($_REQUEST["preference"]);
+    
+    $sql = "UPDATE annonser SET username='$username',email='$email',fullname='$fullname',city='$city',salary='$salary',preference='$preference',aboutme='$aboutme' WHERE id=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$userID]);
+    if($stmt) {
+        print("Informationen uppdaterad!");
+        $_SESSION['username'] = $username;
+        header("Refresh:2; url=profile.php");
+    }
+    else {
+        print("An unexpected error has occurred");
+    }
+}
+    ?>
+
+    <h2>Ta bort din profil & annons</h2>
+    <p>!!KAN INTE ÅTERSTÄLLAS!!<p>
+    <form action="profile.php" method="get">
+    Ditt lösenord: <input type="password" name="confirmPass"><br>
+    <input type="submit" name="delAcc" value="Jag är säker">
+    </form>
 
 </article>
