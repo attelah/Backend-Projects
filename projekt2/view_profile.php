18ec0ce9d0491b1304a27d09db0d5cf6e34052f5
attelah <lahepela@arcada.fi>
2022-02-23 12:33:28 +0200

    profil sidan färdig

--- projekt2/view_profile.php
+++ projekt2/view_profile.php
@@ -14,7 +14,7 @@
         <?php $userID = $row['id']; ?>
         <?php $password = $row['password']; ?>
         <!-- Man kan också inkludera php trots alternative syntax -->
-            <form action="profile.php" method="get">
+            <form action="profile.php" method="post">
             <!-- Om ni sätter php bland html, kom ihåg att inget syns innan print -->
         Användarnamn: <input type="text" name="username" value="<?=$row['username']?>"> <br>
         E-post: <input type="email" name="email" value="<?=$row['email']?>"><br><br>
@@ -22,7 +22,7 @@
         Nuvarande Lösenordet: <input type="password" name="passwordOld"><br>
         Nya Lösenordet:  <input type="password" name="passwordNew"><br><br>
         <h2>MIN ANNONS</h2><br>
-        <!-- Shorthand syntax för php print sparar lite tecken -->
+        <!-- Shorthand syntax för php preint sparar lite tecken -->
         Ditt hela namn: <input type="text" name="fullname" value="<?=$row['fullname']?>"><br>
         Hemstad: <input type="text" name="city" value="<?= $row['city']?>"><br>
         Årlig inkomst: <input type="number" name="salary" value="<?=$row['salary']?>"><br>
@@ -36,7 +36,6 @@
             <option value="5"<?php if($row['preference'] == '5'){echo("selected");}?>>Vill inte specifiera</option>
         </select><br>
         Berätta om dig själv: <input type="text" name="aboutme" value="<?=$row['aboutme']?>"><br>
-        <input type="hidden" name="page" value="modify"> <!--FIXA DET HÄR---------------------------------------------------------------------------------------------->
         <input type="submit" name="skicka" value="Spara Ändringar">
     </form>
     
@@ -53,14 +52,14 @@
         $stmt->execute([$userID]);
         if($stmt) {
             print("Lösenordet uppdaterat!<br>");
-            header("Refresh:2; url=profile.php");
+            header("Refresh:1; url=profile.php");
         }
         else {
             print("An unexpected error has occurred");
         }
         }
         else {
-            print("Ditt gamla lösenord stämmer inte!");
+            print("Ditt gamla lösenord stämmer inte!<br>");
         }
     }
     if(!empty($_REQUEST['username']) || !empty($_REQUEST['fullname']) || !empty($_REQUEST['city']) || !empty($_REQUEST['salary']) || !empty($_REQUEST['preference'])  || !empty($_REQUEST['aboutme']) || !empty($_REQUEST['preference']))
@@ -79,7 +78,7 @@
     if($stmt) {
         print("Informationen uppdaterad!");
         $_SESSION['username'] = $username;
-        header("Refresh:2; url=profile.php");
+        header("Refresh:1; url=profile.php");
     }
     else {
         print("An unexpected error has occurred");
@@ -87,11 +86,29 @@
 }
     ?>
 
-    <h2>Ta bort din profil & annons</h2>
+    <h2>Radera din profil & annons</h2>
     <p>!!KAN INTE ÅTERSTÄLLAS!!<p>
-    <form action="profile.php" method="get">
+    <form action="profile.php" method="post">
     Ditt lösenord: <input type="password" name="confirmPass"><br>
     <input type="submit" name="delAcc" value="Jag är säker">
     </form>
+    <?php
+    if (isset($_REQUEST['delAcc'])) {
+        $pass = hash("sha256", test_input($_REQUEST['confirmPass']));
+    if ($pass === $row['password']) {
+        $sql = "DELETE FROM annonser WHERE id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$userID]);
+        if($stmt) {
+        print("Din profil och annons har blivit raderad."); 
+        header("Refresh:2; url=index.php");
+        session_destroy();
+        }
+        else print("An unexpected error has occurred");
+    } 
+    else print("Inkorrekt lösenord!");
+    }
+
+    ?>
 
 </article>
